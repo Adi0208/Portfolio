b24d3e5b71428c388faf5255e02cddd7976aee9d
Aditya Kumar Gautam <100373039+Adi0208@users.noreply.github.com>
2022-02-28 16:32:52 +0530

    Update main.js
    Fixing navbar error

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-    "use strict";
-  
-    $('ul.nav > li').click(function(e) {
-      
-      $('ul.nav > li').removeClass('active');
-      $(this).addClass('active');
+$(document).ready(function (){
+    $('body').scrollspy({ target: '.navbar', offset: 100 });
+
+    $('.nav-link').on('click', function(){
+        var clickedItem = $(this).attr('href');
+       $('html, body').animate({
+            scrollTop: $(clickedItem).offset().top - 80
+       }, 500);
     });
-  });
+
+});
